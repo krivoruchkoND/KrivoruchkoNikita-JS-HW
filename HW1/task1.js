bd34fd5658ee94658cb706247ce651ee33dcf8f9
krivoruchkoND <lizey@bk.ru>
2020-04-02 18:55:39 +0300

    task1 done

--- HW1/task1.js
+++ HW1/task1.js
@@ -1,1 +1,15 @@
-// in progress
+// Write a function splitAndMerge
+// Function accept 2 parameters: str and sp. 
+// str is a sentence. 
+// sp is a char as separator. 
+// First we need to divide the sentence into words(Use separator space); 
+// and then divide each word into characters(Use separator empty string); 
+// and then merge each characters with the specified sp; at last merge all the words(Use separator space) and return it. 
+// Example:
+// splitAndMerge("My name is John"," ") should return "M y n a m e i s J o h n"
+// splitAndMerge("Hello World!",",") should return "H,e,l,l,o W,o,r,l,d,!"
+export default splitAndMerge = (str, sp) => {
+    const words = str.split(' ');
+    const wordsByCharacters = words.map((word) => word.split(''));
+    return wordsByCharacters.map((word) => word.join(sp)).join(' ');
+}
