ddc0fca74c7fb50a0010fed3118c45ab158d0e43
krivoruchkoND <lizey@bk.ru>
2020-04-02 19:51:33 +0300

    task6 done

--- HW1/task6.js
+++ HW1/task6.js
@@ -1,1 +1,15 @@
-// in progress
+// Write largest and smallest functions that returns the largest and smallest number passed like a argument.
+// Example:
+// largest(2, 0.1, -5, 100, 3) // 100
+// smallest(2, 0.1, -5, 100, 3) // -5
+const largest = (...args) => {
+    let max = args[0];
+    args.forEach((value) => value > max ? max = value : null);
+    return max;
+}
+const smallest = (...args) => {
+    let min = args[0];
+    args.forEach((value) => value < min ? min = value : null);
+    return min;
+}
+export { largest, smallest }
