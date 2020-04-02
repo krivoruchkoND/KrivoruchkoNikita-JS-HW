8c0d815ae4119c6ea872660ebdd31e5ef020e2fc
krivoruchkoND <lizey@bk.ru>
2020-04-02 19:03:33 +0300

    task2 done

--- HW1/task2.js
+++ HW1/task2.js
@@ -1,1 +1,9 @@
-// in progress
+// Write a function convert
+// Convert a hash into an array. Nothing more, Nothing less.
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+// should be converted into
+// [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
+export default convert = (hash) => {
+    const keys = Object.keys(hash);
+    return keys.map((key) => [key, hash[key]]);
+}
