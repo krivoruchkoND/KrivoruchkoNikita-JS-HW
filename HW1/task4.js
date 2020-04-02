a157aa56173e1f73db8db234a2a136956c6f2315
krivoruchkoND <lizey@bk.ru>
2020-04-02 19:36:01 +0300

    task4 done

--- HW1/task4.js
+++ HW1/task4.js
@@ -1,1 +1,9 @@
-// in progress
+// Write a function that takes a sentence (string) and reverses each word in the sentence.
+// Example: 
+// " A fun little challenge! " => " A nuf elttil !egnellahc "
+export default (str) => {
+    return str
+        .split(' ')
+        .map((word) => word.split('').reverse().join(''))
+        .join(' ');
+}
