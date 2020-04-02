a69a235da4b5870252b97cf27a19f3f3fda00498
krivoruchkoND <lizey@bk.ru>
2020-04-02 20:06:17 +0300

    task8 done

--- HW1/task8.js
+++ HW1/task8.js
@@ -1,1 +1,7 @@
-// in progress
+// Write function sum Function expects arbitrary number of digit arguments 
+// and returns compound value of them. Note: function should use recursion
+// Example: 
+// sum(1,3,5,7); //should return 16
+export default sum = (...args) => {
+    return args.reduce((acc, curr) => acc + curr, 0);
+}
