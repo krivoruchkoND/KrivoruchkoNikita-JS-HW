55e79bfe5b8a70012b8df6cf2cd0d46a10e1d212
krivoruchkoND <lizey@bk.ru>
2020-04-06 18:25:31 +0300

    task 9 done

--- HW1/task9.js
+++ HW1/task9.js
@@ -1,1 +1,13 @@
-// in progress
+// Write function countDown. Function expects number and logs values one by one till zero with one second delay.
+// Example: 
+// countDown(3); // 3 2 1 0
+function countDown(seconds) {
+    var timer = setInterval(function () {
+        if (seconds < 0) {
+            clearInterval(timer);
+        } else {
+            console.log(seconds);
+            seconds--;
+        }
+    }, 1000);
+};
