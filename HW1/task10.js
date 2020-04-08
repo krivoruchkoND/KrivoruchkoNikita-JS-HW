2c21d6ba34f7a13aa9e1073e598f68f838ce70dd
krivoruchkoND <lizey@bk.ru>
2020-04-08 22:40:56 +0300

    task10 done

--- HW1/task10.js
+++ HW1/task10.js
@@ -1,1 +1,19 @@
-// in progress
+// Write a polyfill for a .bind() function and save it in Function.prototype.myBind(). 
+// myBind() should work in an exact same way as the usual bind() - take context as a first 
+// parameter and the list of arguments separated by comma.
+// Hint: play with the function in Function.prototype and see what this points to inside it. 
+// Your code should look like:
+// Function.prototype.myBind = function () {
+// // your code here
+// }
+// Example:
+// function addPropToNumber(number) { return this.prop + number; }
+// var bound = addPropToNumber.myBind({ prop: 9 });
+// bound(1) // 10
+Function.prototype.myBind = function(prop) {
+    var f = this;
+    return function() {
+        var args = Number([].slice.call(arguments))
+        return f.call(prop, args)
+    }
+};
